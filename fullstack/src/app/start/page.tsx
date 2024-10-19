c07397255404170f4a417756c10afb4edfb0f0f4
Andrew Hwang <pandy430@gmail.com>
2024-10-19 15:32:04 -0700

    added some stuff

--- fullstack/src/app/start/page.tsx
+++ fullstack/src/app/start/page.tsx
@@ -67,9 +67,7 @@ function Page() {
         initial={{ opacity: 0, y: -20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.8, delay: 0.2 }}
-      >
-
-      </motion.div>
+      ></motion.div>
 
       <motion.div
         className="p-6 z-2 relative"

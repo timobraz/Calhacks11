bf0bcc7e69b93dc3d10938180a8583f7a8fda7d6
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-10-19 03:19:11 -0700

    nextjs kafka

--- python/main.py
+++ python/main.py
@@ -3,7 +3,7 @@ import time
 import selenium
 from kafka import KafkaConsumer, KafkaProducer
 from kafka.admin import KafkaAdminClient, NewTopic
-
+import json
 
 consumer = KafkaConsumer("requests_topic")
 producer = KafkaProducer(bootstrap_servers="localhost:9092")
@@ -14,4 +14,16 @@ kafka_admin_client = KafkaAdminClient(
 
 
 for message in consumer:
-    print("message", message)
+
+    message_string = message.value.decode("utf-8")
+    message_json = json.loads(message_string)
+    print(message_json)
+    for i in range(3):
+        producer.send(
+            message_json["uuid"],
+            bytes(
+                json.dumps({"message": f"created {i}", "preview": f"preview {i}"}),
+                "utf-8",
+            ),
+        )
+        time.sleep(1)

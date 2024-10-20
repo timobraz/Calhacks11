c7a352503525ae72125fa65e826ee461a0ccc409
Timofey Obraztsov <timofeyobraztsov@gmail.com>
2024-10-19 21:35:55 -0700

    conv channel

--- python/main.py
+++ python/main.py
@@ -7,6 +7,13 @@ from pipeline import SpiderPipeline
 async def process_message(message, pipeline, producer):
     message_string = message.value.decode("utf-8")
     message_json = json.loads(message_string)
+    tasks = []
+
+    if message_json.get("action") == "navigate":
+        url = message_json["message"]
+        uuid = message_json.get("uuid")
+        tasks.append(pipeline.navigate(url, producer, uuid))
+
     if message_json.get("action") == "create_browser":
         uuid = message_json["uuid"]
         query = message_json["message"]
@@ -14,12 +21,30 @@ async def process_message(message, pipeline, producer):
         producer.send(
             uuid,
             json.dumps(
-                {"message": f"created selenium browser", "preview": None}
+                {
+                    "message": f"Created Spidey browser",
+                    "preview": None,
+                    "display": True,
+                }
             ).encode("utf-8"),
         )
-        task1 = asyncio.create_task(pipeline.run(query, producer, uuid))
-        task2 = asyncio.create_task(pipeline.take_screenshots(producer, uuid))
-        await asyncio.gather(task1, task2)
+        tasks.append(pipeline.run(query, producer, uuid))
+        tasks.append(pipeline.take_screenshots(producer, uuid))
+
+    if tasks:
+        asyncio.gather(*tasks)
+
+
+async def consume_messages(consumer, pipeline, producer):
+    while True:
+        messages = consumer.poll(timeout_ms=1000)
+        tasks = []
+        for topic_partition, msgs in messages.items():
+            for message in msgs:
+                tasks.append(process_message(message, pipeline, producer))
+        if tasks:
+            await asyncio.gather(*tasks)
+        await asyncio.sleep(0.1)  # Small delay to prevent CPU overuse
 
 
 async def main():
@@ -30,11 +55,7 @@ async def main():
     pipeline = SpiderPipeline()
     print("READY FOR REQUESTS")
 
-    while True:
-        messages = consumer.poll(timeout_ms=1000)
-        for topic_partition, msgs in messages.items():
-            for message in msgs:
-                await process_message(message, pipeline, producer)
+    await consume_messages(consumer, pipeline, producer)
 
 
 if __name__ == "__main__":
